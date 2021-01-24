ad013dd6f5fe904b085d4c29cde50d7ad5c59795
skanushka <skanushka@gmail.com>
2021-01-25 00:59:53 +0530

    finalize work, bug fixes

--- src/main/java/com/xcodel/authservice/config/AuthWebSecurityConfiguration.java
+++ src/main/java/com/xcodel/authservice/config/AuthWebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.xcodel.authservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,10 +8,17 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Configuration
 public class AuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -24,7 +32,6 @@ public class AuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
-                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "**").permitAll()
                 .antMatchers(
@@ -44,7 +51,22 @@ public class AuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/user/**").permitAll()
                 .antMatchers("/**").authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll();
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout(logout -> logout
+                        .permitAll()
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                        .invalidateHttpSession(true)
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            HttpSession session = request.getSession(false);
+                            SecurityContextHolder.clearContext();
+                            session = request.getSession(true);
+                            if (session != null) {
+                                session.invalidate();
+                            }
+                            response.setStatus(HttpServletResponse.SC_OK);
+                        }));
     }
 
     @Bean
@@ -59,7 +81,7 @@ public class AuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
+        auth.inMemoryAuthentication().and().userDetailsService(userDetailsService)
                 .passwordEncoder(getPasswordEncoder());
     }
 }
